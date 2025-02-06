4ae9fd1b72562fd4399c43c4ded3d107411114c3
dusagong <yoonseungjae123@gmail.com>
2025-02-06 13:42:19 +0900

    os별 로컬에서 노티 띄우기

--- lib/notification/notification_service.dart
+++ lib/notification/notification_service.dart
@@ -1,7 +1,13 @@
+import 'dart:math';
+
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationServices {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+      FlutterLocalNotificationsPlugin();
 
 //Permission!
   void requestNotificationPermission() async {
@@ -29,10 +35,56 @@ class NotificationServices {
     return fcmToken!;
   }
 
-  void firebaseInit(){
-    FirebaseMessaging.onMessage.listen((message){
+  void firebaseInit() {
+    FirebaseMessaging.onMessage.listen((message) {
       print(message.notification!.title.toString());
       print(message.notification!.body.toString());
+      showNotification(message);
     });
   }
+
+  Future<void> showNotification(RemoteMessage message) async {
+    AndroidNotificationChannel androidNotificationChannel =
+        AndroidNotificationChannel(
+            Random.secure().nextInt(10000).toString(), 'Flutter Notification',
+            importance: Importance.max);
+
+    AndroidNotificationDetails androidNotificationDetails =
+        AndroidNotificationDetails(androidNotificationChannel.id.toString(),
+            androidNotificationChannel.name.toString(),
+            channelDescription: 'cc',
+            icon: '@mipmap/ic_launcher',
+            importance: Importance.high,
+            ticker: 'ticker');
+
+    DarwinNotificationDetails darwinNotificationDetails =
+        DarwinNotificationDetails(
+      presentAlert: true,
+      presentBadge: true,
+      presentSound: true,
+    );
+
+    NotificationDetails notificationDetails = NotificationDetails(
+        android: androidNotificationDetails, iOS: darwinNotificationDetails);
+
+    Future.delayed(Duration.zero, () {
+      flutterLocalNotificationsPlugin.show(
+          0,
+          message.notification!.title.toString(),
+          message.notification!.body.toString(),
+          notificationDetails);
+    });
+  }
+  // void initLocalNotifications(
+  //     BuildContext context, RemoteMessage message) async {
+  //   var androidInitializationSettings =
+  //       AndroidInitializationSettings('@mipmap/ic_launcher');
+  //   var iosInitializationSettings = DarwinInitializationSettings();
+
+  //   var initializeSettings = InitializationSettings(
+  //       android: androidInitializationSettings, iOS: iosInitializationSettings);
+
+  //   await flutterLocalNotificationsPlugin.initialize(initializeSettings,
+  //       onDidReceiveNotificationResponse: (payload) {});
+  // }
 }
